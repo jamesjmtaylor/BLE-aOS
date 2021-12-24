e779f611168aa55b156175174f55e22355727b36
James Taylor <james.jm.taylor@gmail.com>
2021-12-24 07:58:11 -0800

    Added list item abstraction function.

---
+++ app/src/main/java/com/jamesjmtaylor/blecompose/scan/ListItem.kt
@@ -0,0 +1,15 @@
+﻿package com.jamesjmtaylor.blecompose.scan
+
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun ListItem(name: String, modifier: Modifier) {
+    Text(text = name,
+        fontSize = 16.sp,
+        color= MaterialTheme.colors.secondaryVariant,
+        modifier = modifier)
+}
